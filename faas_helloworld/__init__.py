f14df0f1d03e5f4528099f7bcfd05b49d5981d2c
Stephan Smit <stephan.smit@fudura.nl>
2023-02-02 16:54:09 +0100

    added

--- faas_helloworld/__init__.py
+++ faas_helloworld/__init__.py
@@ -7,7 +7,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
-    
     if not name:
         try:
             req_body = req.get_json()
@@ -15,7 +14,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             pass
         else:
             name = req_body.get('name')
-
+    
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
